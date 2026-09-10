27a3c71d2c386f96bcb4af9e4435478d1e3466d7
komal <preet@12gmail.com>
2026-09-10 20:13:26 +0530

    Update CORS for deployed frontend

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -21,7 +21,10 @@ const app = express();
 
 app.use(
     cors({
-        origin: "http://localhost:5173",
+        origin:[
+             "http://localhost:5173",
+            "https://campuscare-frontend-07rw.onrender.com",
+        ],
         credentials: true,
     })
 );
